645f1a2ec5929d029e705bd570285482708f7cf8
sassinzz13 <diamondassasinzz@gmail.com>
2025-04-29 14:54:49 +0800

    this is the block class

--- block.py
+++ block.py
@@ -3,17 +3,15 @@ import pygame
 # square class to show the block image
 class SquareBlock(pygame.sprite.Sprite):
     def __init__(self, x, y):
-        # Initialize the sprite
+        # initialize the sprite
         pygame.sprite.Sprite.__init__(self)
         self.image = pygame.Surface((50, 50))
-        
         self.rect = self.image.get_rect()
-        self.rect.center = (x, y)
         sprite_sheet_image = pygame.image.load('assets/1.21.4/blocks/dirt.png').convert_alpha()
         self.image = sprite_sheet_image
-        self.rect = self.image.get_rect()  # Recalculate the rectangle to fit the new image
-        self.rect.center = (x, y)  # Keep the position the same
+        self.rect = self.image.get_rect()  
         self.size = pygame.Vector2(200, 150)
+        self.rect.center = (x, y)
         # initialize the self.image to transform the scale of the sprite image
         self.image = pygame.transform.scale(sprite_sheet_image, (int(self.size.x), int(self.size.y)))
 class BlockImage():
